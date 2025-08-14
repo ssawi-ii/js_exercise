df5b8c81d7afe63c6f94ac0fdbd801412abd8c60
A5hisa <themarch2546@gmail.com>
2025-08-14 20:42:51 +0700

    Create 99_beer_song.js

---
+++ javascript/99_beer_song.js
@@ -0,0 +1,18 @@
+var n = 99;
+var march = [];
+function beer(){
+    var x = n--;
+    while (x >= 0) {
+        if (x <= 99 && x > 1){
+            march.push(x +" bottles of beer on the wall, " + x + " bottles of beer. Take one down and pass it around, " + (x-1) + " bottles of beer on the wall.");
+        } else if (x === 1) {
+            march.push(x + " bottle of beer on the wall, "+ x +" bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
+        } else {
+            march.push(" No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+        }
+        x--;
+    }
+    console.log(march);
+}
+beer();
+
